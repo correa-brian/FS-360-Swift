8a143ce57c7f0303b604474cb3bd2eb9dade63fd
Brian <briancorrea@gmail.com>
2016-04-17 16:57:51 -0400

    Parsing JSON

--- DemographicData/DemographicData/ViewControllers/DDFeedViewController.swift
+++ DemographicData/DemographicData/ViewControllers/DDFeedViewController.swift
@@ -7,18 +7,22 @@
 //
 
 import UIKit
+import CoreLocation
 import Alamofire
 
-class DDFeedViewController: DemDataViewController, UITableViewDelegate, UITableViewDataSource {
+class DDFeedViewController: DemDataViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
     
     //MARK: Properties & Functions
+    var locationManager: CLLocationManager!
     var dataTable: UITableView!
+    var areaInfoArray = Array<DDLocation>()
     
     func configureCell(cell: UITableViewCell, indexPath: NSIndexPath) -> UITableViewCell {
         
-        let item = "\(indexPath.row)"
+        let area = self.areaInfoArray[indexPath.row]
         
-        cell.textLabel?.text = item
+        cell.textLabel?.text = ("The Median Income here is: \(area.medianIncome)")
+        cell.detailTextLabel?.text = ("Click to see more stats")
         return cell
     }
     
@@ -34,8 +38,8 @@ class DDFeedViewController: DemDataViewController, UITableViewDelegate, UITableV
         self.dataTable.delegate = self
         self.dataTable.dataSource = self
         
-        self.dataTable.autoresizingMask = .FlexibleTopMargin
-        self.dataTable.separatorStyle = .None
+        self.dataTable.autoresizingMask = .FlexibleHeight
+//        self.dataTable.separatorStyle = .None
         
         view.addSubview(self.dataTable)
         
@@ -45,20 +49,78 @@ class DDFeedViewController: DemDataViewController, UITableViewDelegate, UITableV
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = "http://www.broadbandmap.gov/broadbandmap/"
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.locationManager.requestWhenInUseAuthorization()
+        
+    }
+    
+    //MARK: Location Manager Delegate
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+
+        if (status == .AuthorizedWhenInUse){
+            print("AuthorizedWhenInuse")
+            self.locationManager.startUpdatingLocation()
+        }
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        
+        print("didUpdateLocations: \(locations)")
+        if (locations.count == 0) {
+            return
+        }
+        
+        let loc = locations[0]
+        let now = NSDate().timeIntervalSince1970
+        let locationTime = loc.timestamp.timeIntervalSince1970
+        let delta = now-locationTime
+        
+        print("NOW = \(now)")
+        print("Location Time = \(locationTime)")
+        print("DELTA = \(delta)")
+        
+        if (delta > 10){
+            return
+        }
+        
+        if(loc.horizontalAccuracy > 100){
+            return
+        }
+        
+        self.locationManager.stopUpdatingLocation()
+        print("Found Current Location: \(loc.description)")
+        
+        let lat = "\(loc.coordinate.latitude)"
+        let lng = "\(loc.coordinate.longitude)"
+        
+        let url = "http://www.broadbandmap.gov/broadbandmap/demographic/2014/coordinates?latitude=\(lat)&longitude=\(lng)&format=json"
         
         Alamofire.request(.GET, url, parameters: nil).responseJSON { response in
+            
             if let JSON = response.result.value as? Dictionary<String, AnyObject>{
-                print("\(JSON)")
+//                print("\(JSON)")
+                
+                if let results = JSON["Results"] as? Dictionary<String, AnyObject>{
+//                    print("(\(results)")
+                    
+                    let area = DDLocation()
+                    area.populate(results)
+                    self.areaInfoArray.append(area)
+                }
+                self.dataTable.reloadData()
             }
-        
+                    
         }
+        
     }
     
     //MARK: TableView Delegate Methods
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return self.areaInfoArray.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
